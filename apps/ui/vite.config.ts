a620091b8334ddbaccf05dad80621950beb1a1c1
Vivek Sahu <viveksahu1762@gmail.com>
2025-08-21 14:55:00 +0530

    feat: add Buffer injection and optimize dependencies in Vite config

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -24,6 +24,9 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    inject({
+      Buffer: ['buffer', 'Buffer']
+    }),
     AutoImport({
       imports: ['vue', 'vue-router', '@vueuse/core'],
       dirs: ['./src/composables', './src/stores'],
@@ -62,6 +65,7 @@ export default defineConfig({
     })
   ],
   optimizeDeps: {
+    include: ['buffer'],
     exclude: ['@snapshot-labs/sx'],
     esbuildOptions: {
       target
